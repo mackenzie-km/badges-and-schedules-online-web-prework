3460d1724b8517ecf0c227f17c07a83cfef68285
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-02 22:20:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,8 +11,8 @@ new_array
 end
 
 def assign_rooms(name)
-array.each_with_index(name, )
-new_array.push("Hello, #{name}! You'll be assigned to room #{room_number}!"
+array.each_with_index(name, index)
+new_array.push("Hello, #{name}! You'll be assigned to room #{index}!"
 end
 new_array
 end
