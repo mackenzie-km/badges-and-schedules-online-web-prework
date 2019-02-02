e3833fa77a2c93a528bc291762867ed3acaf5e0b
Mackenzie Moore <macmoore@ucdavis.edu>
2019-02-02 23:32:59 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+new_array = []
+for name in array
+  new_array.push(badge_maker(name))
+end
+new_array
+end
+
+def assign_rooms(array)
+  new_array = []
+array.each_with_index{|name, index| 
+new_array.push("Hello, #{name}! You'll be assigned to room #{index+1}!")}
+return new_array
+end
+
+
+def printer(array)
+array.each_with_index{|name, index|
+badge_batch = batch_badge_creator(array)
+room_assignments = assign_rooms(array)
+  puts badge_batch[index]
+  puts room_assignments[index]
+}
+end
